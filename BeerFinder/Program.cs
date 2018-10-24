d330e7e6d3e6c0d75f543228c7310bd15cd08ab5
Adam Seal <aseal33@gmail.com>
2018-10-24 13:07:50 -0400

    Initial Commit

--- BeerFinder/Program.cs
+++ BeerFinder/Program.cs
@@ -6,7 +6,7 @@ namespace BeerFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Beer!");
         }
     }
 }
